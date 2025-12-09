87c3aa1b7c11f62d0d2ca42d356403e1138c3278
PAVLOS GRATSANIS <p3230036@aueb.gr>
2025-12-10 00:17:27 +0200

    update AddressTest.java

--- app/app/src/test/java/gr/softeng/team21/domain/AddressTest.java
+++ app/app/src/test/java/gr/softeng/team21/domain/AddressTest.java
@@ -1,63 +1,65 @@
-import org.junit.After;
-import org.junit.jupiter.api.*;
+package gr.softeng.team21.domain;
+
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AddressTest {
     private Address address;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         address = new Address("Street", "10", "Athens", "Greece", "12345");
     }
 
     @Test
-    void getAndSetStreetTest() {
-        Assertions.assertEquals("Street", address.getStreet());
+    public void getAndSetStreetTest() {
+        assertEquals("Street", address.getStreet());
         address.setStreet("Other Street");
-        Assertions.assertEquals("Other Street", address.getStreet());
+        assertEquals("Other Street", address.getStreet());
     }
 
     @Test
-    void getAndSetZipcodeTest() {
-        Assertions.assertEquals("12345", address.getZipcode());
+    public void getAndSetZipcodeTest() {
+        assertEquals("12345", address.getZipcode());
         address.setZipcode("54321");
-        Assertions.assertEquals("54321", address.getZipcode());
+        assertEquals("54321", address.getZipcode());
     }
 
     @Test
-    void getAndSetCountryTest() {
-        Assertions.assertEquals("Greece", address.getCountry());
+    public void getAndSetCountryTest() {
+        assertEquals("Greece", address.getCountry());
         address.setCountry("Italy");
-        Assertions.assertEquals("Italy", address.getCountry());
+        assertEquals("Italy", address.getCountry());
     }
 
     @Test
-    void getAndSetCityTest() {
-        Assertions.assertEquals("Athens", address.getCity());
+    public void getAndSetCityTest() {
+        assertEquals("Athens", address.getCity());
         address.setCity("Thessaloniki");
-        Assertions.assertEquals("Thessaloniki", address.getCity());
+        assertEquals("Thessaloniki", address.getCity());
     }
 
     @Test
-    void getAndSetNumberTest() {
-        Assertions.assertEquals("10", address.getNumber());
+    public void getAndSetNumberTest() {
+        assertEquals("10", address.getNumber());
         address.setNumber("20");
-        Assertions.assertEquals("20", address.getNumber());
+        assertEquals("20", address.getNumber());
     }
 
     @Test
     public void equalsSameAddressTest() {
         Address address = new Address("A", "1", "C", "E", "D");
-        Assertions.assertEquals(address, address);
+        assertEquals(address, address);
     }
 
     @Test
     public void equalsNullAndOtherClassTest() {
         Address address = new Address("A", "1", "C", "E", "D");
-        Assertions.assertNotEquals(null, address);
+        assertNotEquals(null, address);
 
 
-        Assertions.assertNotEquals(new Object(), address);
+        assertNotEquals(new Object(), address);
     }
 
     @Test
@@ -65,8 +67,8 @@ public class AddressTest {
         Address address1 = new Address("Ermou", "10", "Athens", "Greece", "10563");
         Address address2 = new Address("Ermou", "10", "Athens", "Greece", "10563");
 
-        Assertions.assertEquals(address1, address2);
-        Assertions.assertEquals(address1.hashCode(), address2.hashCode());
+        assertEquals(address1, address2);
+        assertEquals(address1.hashCode(), address2.hashCode());
     }
 
 
@@ -76,19 +78,19 @@ public class AddressTest {
         Address different;
 
         different = new Address("B", "1", "C", "E", "D");
-        Assertions.assertNotEquals(base, different, "Should differ in Street");
+        assertNotEquals(base, different);
 
         different = new Address("A", "2", "C", "E", "D");
-        Assertions.assertNotEquals(base, different, "Should differ in Number");
+        assertNotEquals(base, different);
 
         different = new Address("A", "1", "D", "E", "D");
-        Assertions.assertNotEquals(base, different, "Should differ in City");
+        assertNotEquals(base, different);
 
         different = new Address("A", "1", "C", "F", "D");
-        Assertions.assertNotEquals(base, different, "Should differ in Country");
+        assertNotEquals(base, different);
 
         different = new Address("A", "1", "C", "E", "E");
-        Assertions.assertNotEquals(base, different, "Should differ in Zipcode");
+        assertNotEquals(base, different);
     }
 
 
@@ -98,8 +100,8 @@ public class AddressTest {
         Address address1 = new Address(null, null, null, null, null);
         Address address2 = new Address(null, null, null, null, null);
 
-        Assertions.assertEquals(address1, address2, "Null fields should be equal");
-        Assertions.assertEquals(0, address1.hashCode());
+        assertEquals(address1, address2);
+        assertEquals(0, address1.hashCode());
     }
 
     @Test
@@ -107,7 +109,7 @@ public class AddressTest {
         Address nonNullAddress = new Address("A", "1", "C", "E", "D");
         Address nullAddress = new Address(null, null, null, null, null);
 
-        Assertions.assertNotEquals(nonNullAddress, nullAddress, "Non-null should not equal null");
+        assertNotEquals(nonNullAddress, nullAddress);
     }
 
     @Test
@@ -115,17 +117,17 @@ public class AddressTest {
         Address nullAddress = new Address(null, null, null, null, null);
         Address nonNullAddress = new Address("A", "1", "C", "E", "D");
 
-        Assertions.assertNotEquals(nullAddress, nonNullAddress, "Null should not equal non-null");
+        assertNotEquals(nullAddress, nonNullAddress);
     }
 
     @Test
     public void equalsPartialNullTest() {
-        Address address1 = new Address("Ermou", "10", "Athens", "Greece", null); // Zipcode is null
-        Address address2 = new Address("Ermou", "10", "Athens", "Greece", "10563"); // Zipcode is non-null
+        Address address1 = new Address("Ermou", "10", "Athens", "Greece", null);
+        Address address2 = new Address("Ermou", "10", "Athens", "Greece", "10563");
 
-        Assertions.assertNotEquals(address1, address2, "Should differ if only one zipcode is null");
+        assertNotEquals(address1, address2);
 
-        Assertions.assertNotEquals(address2, address1, "Should differ (Symmetry check)");
+        assertNotEquals(address2, address1);
     }
 
     @Test
@@ -133,15 +135,15 @@ public class AddressTest {
         Address address1 = new Address("A", "1", "C", null, "D");
         Address address2 = new Address("B", "1", "C", null, "D");
 
-        Assertions.assertNotEquals(address1, address2, "Should differ in Street even with common null Country");
+        assertNotEquals(address1, address2);
 
-        Assertions.assertNotEquals(address1.hashCode(), address2.hashCode());
+        assertNotEquals(address1.hashCode(), address2.hashCode());
     }
 
     @Test
     public void hashCodeNullAllPropertiesIsZeroTest(){
         Address address1 = new Address();
-        Assertions.assertEquals(0,address1.hashCode());
+        assertEquals(0,address1.hashCode());
     }
 
     @Test
@@ -149,9 +151,9 @@ public class AddressTest {
         Address address1 = new Address("Street", "10", "Athens", "Greece", "12345");
         Address address2 = new Address("Street", "10", "Athens", "Greece", "12345");
 
-        Assertions.assertEquals(address1.hashCode(),address1.hashCode());
+        assertEquals(address1.hashCode(),address1.hashCode());
 
-        Assertions.assertEquals(address1.hashCode(),address2.hashCode());
+        assertEquals(address1.hashCode(),address2.hashCode());
     }
 
     @Test
@@ -159,7 +161,7 @@ public class AddressTest {
         Address address1 = new Address("Street1", "11", "Athens", "Greece", "12345");
         Address address2 = new Address("Street2", "12", "Athens", "Greece", "34214");
 
-        Assertions.assertNotEquals(address1.hashCode(),address2.hashCode());
+        assertNotEquals(address1.hashCode(),address2.hashCode());
     }
 
 }
